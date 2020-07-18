3456e74234bce91f38c207c841dc560859c1ae65
ricardogusi <ricardogusi@hotmail.com>
2020-07-18 09:34:05 -0300

    Updates

---
+++ precache-manifest.d9bdc96e7e63129351fc23a6bbdb8cb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9ac0e1132fb95230112546e764a9fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79968e7ca78c7820e565",
+    "url": "/static/css/main.0fa96d0a.chunk.css"
+  },
+  {
+    "revision": "41e04e2f95f3b632cb29",
+    "url": "/static/js/2.bdb6dcf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdb6dcf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79968e7ca78c7820e565",
+    "url": "/static/js/main.44152c1e.chunk.js"
+  },
+  {
+    "revision": "fb3b8747f7d863d5053e",
+    "url": "/static/js/runtime-main.5ea4d90f.js"
+  },
+  {
+    "revision": "eb556acdf6bd54c35fabaf057defa5a1",
+    "url": "/static/media/bg.eb556acd.jpg"
+  }
+]);
