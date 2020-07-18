d766d3abcb020acc17536b22af900dabcfea542f
ricardogusi <ricardogusi@hotmail.com>
2020-07-18 10:09:53 -0300

    Updates

---
+++ precache-manifest.9ab2b3a494f8e05f3e27a07d0c63b1ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca94407dccb9c7de60892be60cc4721",
+    "url": "/quoteGenerator/index.html"
+  },
+  {
+    "revision": "eef35a14bf7ed2b03b35",
+    "url": "/quoteGenerator/static/css/main.5c3db2e9.chunk.css"
+  },
+  {
+    "revision": "35443b66eee73b039100",
+    "url": "/quoteGenerator/static/js/2.d2e76e54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quoteGenerator/static/js/2.d2e76e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef35a14bf7ed2b03b35",
+    "url": "/quoteGenerator/static/js/main.553e6e33.chunk.js"
+  },
+  {
+    "revision": "9fafccb24eb5ed99a928",
+    "url": "/quoteGenerator/static/js/runtime-main.6ea57a40.js"
+  },
+  {
+    "revision": "eb556acdf6bd54c35fabaf057defa5a1",
+    "url": "/quoteGenerator/static/media/bg.eb556acd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.933515fa6d7e3e57780268633cb27e9c.js"
+  "/quoteGenerator/precache-manifest.9ab2b3a494f8e05f3e27a07d0c63b1ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quoteGenerator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
